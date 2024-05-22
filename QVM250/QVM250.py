b3098e53c583b1c751ef2ab7607a529c21e427be
T4y1oR <maxxu.979@gmail.com>
2024-05-22 09:35:26 +0800

    20240522

---
+++ QVM250/QVM250.py
@@ -0,0 +1,128 @@
+import os
+import sys
+import random
+import subprocess
+import string
+import hashlib
+from PIL import Image
+import pywin32_system32
+import time
+
+
+def banner():
+ print('''
+           +-+ +-+ +-+ +-+ +-+ +-+
+           |Q| |V| |M| |2| |5| |0|
+           +-+ +-+ +-+ +-+ +-+ +-+
+
+    Github: https://github.com/T4y1oR/RingQ
+          E.g: QVM250.exe main.exe
+''')
+
+def creatico():
+
+    main_folder = "main"  
+    random_folder = "random"  
+
+    file_list = os.listdir(random_folder)
+    for file_name in file_list:
+        os.remove(os.path.join(random_folder, file_name))
+
+    for filename in os.listdir(main_folder):
+        if filename.endswith('.ico'):
+            with Image.open(os.path.join(main_folder, filename)) as image:
+                try:
+                    # random_width = random.randint(1, 48)
+                    # random_height = random.randint(1, 48)
+
+                    resized_image = image.resize((48, 48))
+
+                    pixel_data = list(resized_image.getdata())
+                    modified_pixel_data = [(r + random.randint(-11, 11), g + random.randint(-11, 11), b + random.randint(-11, 11), a) for (r, g, b, a) in pixel_data]
+                    modified_image = Image.new(resized_image.mode, resized_image.size)
+                    modified_image.putdata(modified_pixel_data)
+
+                    new_created_date = str(random.randint(0, int(time.time())))  
+                    file_stats = os.stat(os.path.join(main_folder, filename))
+                    os.utime(os.path.join(main_folder, filename), (file_stats.st_atime, int(new_created_date)))
+
+                    hash_value = hashlib.md5(str(random.random()).encode()).hexdigest()
+
+                    new_filename = os.path.join(random_folder, f"{hash_value}.ico")
+                    modified_image.save(new_filename)
+
+                    # print(f"已处理文件: {filename} -> {new_filename}")
+
+                except:
+                    pass
+
+
+def generate_random_string(length):
+    letters = string.ascii_letters + string.digits  
+    random_string = ''.join(random.choice(letters) for _ in range(length))
+    return random_string
+
+def set_version_info(exe_path):
+    rcedit_path = "rcedit-x64.exe"
+    file_description = generate_random_string(50)
+    company_name = generate_random_string(50)
+    legal_copyright = generate_random_string(50)
+    product_name = generate_random_string(50)
+    file_version = "0.0.1"
+    product_version = "0.0.1"
+
+    command = [
+        rcedit_path,
+        exe_path,
+        "--set-version-string", "FileDescription", file_description,
+        "--set-file-version", file_version,
+        "--set-product-version", product_version,
+        "--set-version-string", "CompanyName", company_name,
+        "--set-version-string", "LegalCopyright", legal_copyright,
+        "--set-version-string", "FileVersion", "3.10",
+        "--set-version-string", "ProductName", product_name
+    ]
+
+    subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+def add_random_icons(exename):
+    savaname = generate_random_string(15)
+    icon_folder = "random"  
+    icon_files = os.listdir(icon_folder)  
+    random_icofiles = random.sample(icon_files, 2)  
+
+    randomico1 = random_icofiles[0]
+    randomico2 = random_icofiles[1]
+
+    command1 = [
+        "ResourceHacker.exe",
+        "-open", exename,
+        "-save", f"{savaname}.exe",
+        "-action", "addskip",
+        "-res", f"{icon_folder}/{randomico1}",
+        "-mask", f"ICONGROUP,{generate_random_string(15)}"
+    ]
+    subprocess.run(command1, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+    set_version_info(f"{savaname}.exe")
+
+    return savaname
+
+if __name__ == '__main__':
+    banner()
+    try:
+
+        exename = sys.argv[1]
+        creatico()
+        time.sleep(3)
+        savaname = add_random_icons(exename)
+        if not os.path.isfile(f"{savaname}.exe"):
+            print("[-] 生成失败！ 请重新尝试~ ")
+        else:
+            print("[+] 伪造资源成功！")
+            print('[+] Enjoy! ' + f"{savaname}.exe")
+
+    except:
+        pass
--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 
 1. 你的任意exe工具，放在与Create.exe 同文件夹下运行即可，会生成一个混淆的main.txt文件。
    
-   Create.exe 此程序仅用于生成main.txt文件，可能存在报毒(c++你懂的 helloworld都报毒) 添加白名单or虚拟机断网使用都可以。
+   <u>*Create.exe 此程序仅用于生成main.txt文件，可能存在报毒(c++你懂的) 添加白名单or虚拟机断网使用都可以。*</u>
 
    ```
    Create.exe mimikatz.exe
@@ -42,15 +42,19 @@
    ![](https://github.com/T4y1oR/RingQ/blob/main/images/image-20240511172315793.png)
    ![](https://github.com/T4y1oR/RingQ/blob/main/images/image-20240511172315791.png)
 
-### **微步**
+### **微步**（2024.05.22更新）
 ![](https://github.com/T4y1oR/RingQ/blob/main/images/image-20240511162750465.png)
-### **360**（2024.05.21更新）
+### **360**（2024.05.22更新）
 ![](https://github.com/T4y1oR/RingQ/blob/main/images/360.png)
 ![](https://github.com/T4y1oR/RingQ/blob/main/images/image-20240511165253870.png)
 
-### **Windows Defender**（2024.05.21更新）
+### **Windows Defender**（2024.05.22更新）
 ![](https://github.com/T4y1oR/RingQ/blob/main/images/DF.png)
-### **火绒6.0**（2024.05.21更新）
+
+### **卡巴斯基**（2024.05.22更新）
+![](https://github.com/T4y1oR/RingQ/blob/main/images/kb.png)
+
+### **火绒6.0**（2024.05.22更新）
 ![](https://github.com/T4y1oR/RingQ/blob/main/images/huorong.png)
 
 
@@ -61,19 +65,32 @@
 
 ![](https://github.com/T4y1oR/RingQ/blob/main/images/Snipaste_2024-05-19_18-15-10.png)
 
-### 该报毒不是主程序源码被360干掉，此程序我在2024.01月份就一直在实战中使用，多点Star后续出专项bypass QVM202脚本。
+<u>*该报毒不是主程序源码被360干掉，此程序我在2024.01月份就一直在实战中使用。*</u>
 
 ![](https://github.com/T4y1oR/RingQ/blob/main/images/Snipaste_2024-05-19_18-54-11.png)
 
+### QVM250（bypass 360 QVM202）
+
+[](https://github.com/T4y1oR/RingQ/QVM250)
+![](https://github.com/T4y1oR/RingQ/blob/main/images/QVM250.gif)
 
 
-## 承诺无任何后门，后续如报毒属于正正正正正常现象，提issues（报毒截图and时间环境信息）耐心等待更新即可。tips:  Star后更新更快！
+<u>*图标支持自定义，存放在QVM250/main文件夹内即可。*</u>
+
+
+
+
+
+
+## 承诺无任何后门，放心食用。后续如报毒属于正正正正正常现象，提issues（报毒截图and时间环境信息）耐心等待更新即可。tips:  Star后更新更快！
 
 ## 项目仅供进行学习研究，切勿用于任何非法未授权的活动，如个人使用违反安全相关法律，后果与本人无关。
 
 
 
 ### 更新日志
+2024.05.22  QVM250发布，bypass 360QVM202
+
 2024.05.14-21 360QVM202报毒 已更新，这个报毒自行更换图标，添加签名...  ~~如果持续出现这个问题，多点star出个专项bypass QVM202脚本~~ 
            Releases新增RingQ.exe原编译出的main.exe文件，方便大家自定义修改降熵
 
@@ -83,8 +100,9 @@
 
 ### TODO
 
-- [ ] 不让我传fscan 我直接干掉AV？
-- [ ] 强化版unhook
-- [ ] bypass QVM202
+- [ ] 不让我传fscan 直接干掉AV？
+- [ ] unhook
+- [ ] 远程加载
+- [x] bypass QVM202
 - [ ] ...
 
